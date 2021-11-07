4cf90080e4881a1af667f61eb22386ce58110ab2
trobdev <trobdev@trobsec.com>
2021-11-07 14:24:33 -0800

    eliminated provider resource

--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
 # --- s3/providers.tf ---
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
